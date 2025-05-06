3fb7933d4650bde09e37299cf7b014326ce53f24
saniya <saniyaelzabeth1543@gmail.com>
2025-05-06 12:14:42 +0530

    product visibility day 2.1

--- controller/website_sale.py
+++ controller/website_sale.py
@@ -1,32 +1,34 @@
 from odoo.addons.website_sale.controllers.main import WebsiteSale
-from odoo.http import request, route
+
+from addons.website_sale.controllers.main import TableCompute
+from odoo.http import request
 from odoo import http
+from odoo.tools import lazy
+from odoo.addons.website_sale.controllers import main
+
 
 class WebsiteSaleCustom(WebsiteSale):
+
     @http.route([
-           '/shop',
-           '/shop/page/<int:page>',
-           '/shop/category/<model("product.public.category"):category>',
-          '/shop/category/<model("product.public.category"):category>/page/<int:page>',
-       ], type='http', auth="public", website=True)
+        '/shop',
+        '/shop/page/<int:page>',
+        '/shop/category/<model("product.public.category"):category>',
+        '/shop/category/<model("product.public.category"):category>/page/<int:page>',
+    ], type='http', auth="public", website=True)
     def shop(self, page=0, category=None, search='', **post):
-        print("hi")
-        result = super().shop(page=page, category=category, search=search, **post)
+        response = super(WebsiteSaleCustom, self).shop(page=page, category=category, search=search, **post)
 
         user = request.env.user
         partner = user.partner_id
-
         domain = []
-        if partner.product_ids:
+        if partner.allowed_products == 'product' and partner.product_ids:
             domain = [('id', 'in', partner.product_ids.ids)]
-            print(partner.product_ids)
-        elif partner.product_category_ids:
-            domain = [('product_category_ids', 'in', partner.product_category_ids.ids)]
+            print(partner.product_ids.ids)
+        elif partner.allowed_products == 'product_categories' and partner.product_category_ids:
+            domain = [('categ_id', 'in', partner.product_category_ids.ids)]
 
         if domain:
             filtered_products = request.env['product.template'].search(domain)
-            print(result.qcontext['products'])
-            result.qcontext['products'] = filtered_products
-            print(result.qcontext['products'])
-
-        return result
+            # response.qcontext['products'] = filtered_products
+            response.qcontext['bins'] = lazy(lambda: TableCompute().process(filtered_products, ppg, ppr))
+        return response
